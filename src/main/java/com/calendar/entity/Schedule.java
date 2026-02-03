ee477f02f87cc80fe341d0012ccb12b36e089633
hyeonmin02 <hyenmin5@gmail.com>
2026-02-03 22:35:16 +0900

    updateSchedule 일정 제목,작성자명만 남기고 삭제+@Column 추가

--- src/main/java/com/calendar/entity/Schedule.java
+++ src/main/java/com/calendar/entity/Schedule.java
@@ -14,9 +14,17 @@ public class Schedule extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 고유 식별자(ID)
+
+    @Column(nullable = false)
     private String title; // 일정 제목
+
+    @Column(nullable = false)
     private String content; // 일정 내용
+
+    @Column(nullable = false)
     private String writer; // 작성자명
+
+    @Column(nullable = false)
     private String password; // 비밀번호
 
     public Schedule(String title, String content, String writer, String password) {
@@ -26,10 +34,8 @@ public class Schedule extends BaseEntity {
         this.password = password;
     }
 
-    public void updateSchedule(String title, String content, String writer, String password) {
-        this.title = title;
-        this.content = content;
+    public void updateSchedule(String title, String writer) { // 과제의 요구사항: 일정 제목, 작성자명만 수정가능하다를 수용
+        this.title = title; // TODO 비밀번호도 요청을 해야하는데 따로 포함하지않아도되나?
         this.writer = writer;
-        this.password = password;
     }
 }
