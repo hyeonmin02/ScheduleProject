bf54945903f01b9d6e7d6264f1181d52889cdebb
hyeonmin02 <hyenmin5@gmail.com>
2026-02-05 11:45:04 +0900

    PutMapping -> PatchMapping으로 변경

--- src/main/java/com/calendar/controller/ScheduleController.java
+++ src/main/java/com/calendar/controller/ScheduleController.java
@@ -1,6 +1,6 @@
 package com.calendar.controller;
 
-import com.calendar.dto.*;
+import com.calendar.scheduledto.*;
 
 import com.calendar.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
@@ -16,32 +16,37 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping("/schedules") // 일정 생성
-    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
+    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request)
+    {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.createSchedule(request));
     }
 
-    @GetMapping("/schedules") // 전체 일정 조회  @PathVariable과 @RequestBody 필요없음 URL에 writer라는 값이 잇으면 받아서 쓰고 아니면 null로 둬라
-    public ResponseEntity<List<GetScheduleResponse>> getSchedules(@RequestParam(required = false) String writer) {
+    @GetMapping("/schedules") // 전체 일정 조회 URL에 writer라는 값이 잇으면 받아서 쓰고 아니면 null로 둬라
+    public ResponseEntity<List<GetScheduleResponse>> getSchedules(@RequestParam(required = false) String writer)
+    {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getSchedules(writer));
     }
 
     @GetMapping("/schedules/{scheduleId}") // 선택 일정 조회
-    public ResponseEntity<GetScheduleResponse> getSchedule(@PathVariable Long scheduleId) {
+    public ResponseEntity<GetScheduleInCommentResponse> getSchedule(@PathVariable Long scheduleId)
+    {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getSchedule(scheduleId));
     }
 
-    @PutMapping("/schedules/{scheduleId}") // 선택 일정 수정
-    public ResponseEntity<UpdateScheduleResponse> updateSchedule
-            (@PathVariable Long scheduleId,
-             @RequestBody UpdateScheduleRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.updateSchedule(scheduleId,request));
+    @PatchMapping("/schedules/{scheduleId}") // 선택 일정 수정
+    public ResponseEntity<UpdateScheduleResponse> updateSchedule(
+            @PathVariable Long scheduleId,
+             @RequestBody UpdateScheduleRequest request)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.updateSchedule(scheduleId, request));
     }
 
     @DeleteMapping("/schedules/{scheduleId}") // 선택 일정 삭제
-    public ResponseEntity<Void>deleteSchedule(
+    public ResponseEntity<Void> deleteSchedule(
             @PathVariable Long scheduleId,
             @RequestBody DeleteScheduleRequest request)
-    { scheduleService.deleteSchedule(scheduleId, request);
+    {
+        scheduleService.deleteSchedule(scheduleId, request);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
